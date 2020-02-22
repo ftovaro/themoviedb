82075762074618d07f55bd288e9b0145b33bf5c1
Felipe Tovar <f.tovar12@gmail.com>
2020-02-22 10:49:31 -0500

    Add token auth and pry gem

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -2,11 +2,21 @@ module Api
   module V1
     class MoviesController < Api::BaseController
       skip_before_action :verify_authenticity_token
-      # before_action :authenticate_token
-      # respond_to :json
+      before_action :authenticate_token
 
       def create
         render json: { message: "Movie created" }, status: 200
+      end
+
+      private
+
+      def authenticate_token
+        token = request.headers["X-Api-Key"]
+        render json: { status: "401", title: "Unauthorized" }, status: :unauthorized if token != authorized_token
+      end
+
+      def authorized_token
+        "SMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
       end
     end
   end
--- spec/api/v1/movies_controller_spec.rb
+++ spec/api/v1/movies_controller_spec.rb
@@ -3,9 +3,25 @@ require "rails_helper"
 describe Api::V1::MoviesController do
 
   describe "#create" do
+    context "when the token is missing" do
+      it "returns 401" do
+        post "/api/v1/movies", params: {}
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
     context "when send a valid payload" do
+      before(:each) do
+        expect_any_instance_of(Api::V1::MoviesController).to receive(:authorized_token).and_return("sampletoken")
+      end
+
       it "returns 200" do
-        post "/api/v1/movies", params: {}, as: :json
+        post "/api/v1/movies", params: {}, headers: { "X-Api-Key": "sampletoken" }
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "sends a tmdb_id and returns 200" do
+        post "/api/v1/movies", params: { movie: { tmdb_id: 1 } }, headers: { "X-Api-Key": "sampletoken" }
         expect(response).to have_http_status(:ok)
       end
     end
