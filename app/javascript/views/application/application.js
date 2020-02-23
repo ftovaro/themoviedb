884a951a10599a0cd76b597f3eab53f00a58e722
Felipe Tovar <f.tovar12@gmail.com>
2020-02-23 13:00:43 -0500

    Add vuetify lib

--- app/javascript/views/application/application.js
+++ app/javascript/views/application/application.js
@@ -1,7 +1,11 @@
 import Vue from 'vue/dist/vue.esm';
+import vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
 
 import movieList from './sections/movie_list.vue';
 
+Vue.use(vuetify)
 /* Init Vue App */
 document.addEventListener('DOMContentLoaded', () => {
 
